0ff93a12d51103a1e5cca2c8b531a023d1763cb8
vishwaradhyasalimath <vishwasalimath7@gmail.com>
2025-08-29 23:57:44 +0530

    Add files via upload

---
+++ selanium_webscrapping/selnium.py
@@ -0,0 +1,38 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+
+s= Service('C:/Users/vishw/OneDrive/Desktop/chromedriver-win64/chromedriver.exe')
+
+driver = webdriver.Chrome(service = s)
+
+driver.get('https://www.smartprix.com/mobiles')
+time.sleep(2)
+driver.find_element(by=By.XPATH,value='/html/body/div/main/aside/div/div[5]/div[2]/label[1]/input').click()
+time.sleep(2)
+driver.find_element(by=By.XPATH,value='/html/body/div/main/aside/div/div[5]/div[2]/label[2]/input').click()
+time.sleep(2)
+
+old_height = driver.execute_script('return document.body.scrollHeight')
+while True:
+    driver.find_element(by=By.XPATH,value='//*[@id="app"]/main/div[1]/div[2]/div[3]').click()
+    time.sleep(2)
+
+    new_height = driver.execute_script('return document.body.scrollHeight')
+
+    print(old_height)
+    print(new_height)
+    if new_height == old_height:
+        break
+    old_height=new_height
+
+
+html = driver.page_source
+with open('mobile.html','w',encoding='utf-8') as f:
+    f.write(html)
+
+
